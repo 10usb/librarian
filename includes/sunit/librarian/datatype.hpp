4095b3800a6532105deef6185ac23a714ad1dccb
10usb <t.bruins@gmail.com>
2020-10-03 15:06:49 +0200

    Added templates to support easy assigning of primitives

--- includes/sunit/librarian/datatype.hpp
+++ includes/sunit/librarian/datatype.hpp
@@ -12,11 +12,29 @@ namespace sunit::librarian {
 
     void assign(const void *source, DataType srcType, int srcLength, void *destination, DataType dstType, int dstLength);
 
+    // Integers
+    inline void assign(char value, void *destination, DataType dstType, int dstLength){
+        assign(&value, Integer, sizeof(char), destination, dstType, dstLength);
+    }
+    inline void assign(short value, void *destination, DataType dstType, int dstLength){
+        assign(&value, Integer, sizeof(short), destination, dstType, dstLength);
+    }
     inline void assign(long value, void *destination, DataType dstType, int dstLength){
         assign(&value, Integer, sizeof(long), destination, dstType, dstLength);
     }
+    inline void assign(long long value, void *destination, DataType dstType, int dstLength){
+        assign(&value, Integer, sizeof(long long), destination, dstType, dstLength);
+    }
+
+    // Floats
+    inline void assign(float value, void *destination, DataType dstType, int dstLength){
+        assign(&value, Float, sizeof(float), destination, dstType, dstLength);
+    }
     inline void assign(double value, void *destination, DataType dstType, int dstLength){
         assign(&value, Float, sizeof(double), destination, dstType, dstLength);
+    }
+    inline void assign(long double value, void *destination, DataType dstType, int dstLength){
+        assign(&value, Float, sizeof(long double), destination, dstType, dstLength);
     }
 
     int compare(const void *left, const void *right, DataType type, int length);
